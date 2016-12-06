0307b22611f8aee20213f330fc3a114fe91832af
Felipe Sandoval <felipesandovalsibada@outlook.com>
2016-12-06 20:49:29 +0100

    funciona todo lo basico. falta correccion en los puertos del log.

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -70,17 +70,24 @@ def send_to_uaserver(ip, port, data):
     """Initiates a socket to send to my UA server."""
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((ip, int(port)))
-    my_socket.send(bytes(data, 'utf-8'))
-    log_connect = [ip, port]
-    log_data = (" ").join(data.split())
-    sents_log(log_connect, log_file, log_data)
-    recieved = my_socket.recv(1024).decode('utf-8')
-    #print(data.split()[0])
-    print("-- RECIEVED REQUEST --\r\n" + recieved)
-    recieved_log(log_connect, log_file, (" ").join(recieved.split()))
-    my_socket.close()
+    try:
+        my_socket.connect((ip, int(port)))
+        my_socket.send(bytes(data, 'utf-8'))
+        log_connect = [ip, port]
+        log_data = (" ").join(data.split())
+        sents_log(log_connect, log_file, log_data)
+        recieved = ""
+        if data.split()[0] != "ACK":
+            recieved = my_socket.recv(1024).decode('utf-8')
+            print("-- RECIEVED REQUEST --\r\n" + recieved)
+            recieved_log(log_connect, log_file, (" ").join(recieved.split()))
+        my_socket.close()
+    except ConnectionRefusedError:
+        print("-- ALERT! -- Connection Refused in port " + port +
+              ": UAServer port not found.\n")
+        recieved = "SIP/2.0 504 Server Time-out\r\n\r\n"
     return recieved
+
 
 def make_users(data):
     """Making my users DATABASE .txt file."""
@@ -90,7 +97,6 @@ def make_users(data):
         Found = False
         lines = users_file.readlines()
         for line in lines:
-            print(lines.index(line))
             if line.split()[0] == data.split()[0]:
                 Found = True
                 line.replace(line.split()[3], data.split()[3])
@@ -116,17 +122,18 @@ def find_password(user):
     except FileNotFoundError:   # When the file doesn't exists. NEED PASSWORDS.
         pasword = "00null00"
     return password
-    
+
+
 class SIPHandler(socketserver.DatagramRequestHandler):
     """Main handler of SIP responses."""
     my_dic = {}         # My active client dic.
     exist_file = True
-    nonce = []
+    nonce = []          # Making a random number every time.
 
     def json2registered(self):
         """Method to look if there is an over-existing json file."""
         try:
-            with open("registered.json", "r") as data_file:
+            with open("active_clients.json", "r") as data_file:
                 self.my_dic = json.load(data_file)
                 self.exist_file = True
         except:
@@ -134,14 +141,9 @@ class SIPHandler(socketserver.DatagramRequestHandler):
 
     def register2json(self):
         """Method to write a json file of my_dic."""
-        if not self.exist_file or len(self.my_dic) == 0:
-            with open("registered.json", "w") as outfile:
-                json.dump(self.my_dic, outfile, indent=4, sort_keys=True,
-                          separators=(',', ':'))
-        else:
-            with open("registered.json", "r+") as outfile:
-                json.dump(self.my_dic, outfile, indent=4, sort_keys=True,
-                          separators=(',', ':'))
+        with open("active_clients.json", "w") as outfile:
+            json.dump(self.my_dic, outfile, indent=4, sort_keys=True,
+                      separators=(',', ':'))
 
     def handle(self):
         """Handler to manage incoming users SIP request."""
@@ -150,12 +152,10 @@ class SIPHandler(socketserver.DatagramRequestHandler):
         line_hash = (" ").join(line_str)
         recieved_log(self.client_address, log_file, line_hash)
 
-    # LO QUE RECIBO REVISAR SI DEBERIA ESTAR AQUI O ABAJO
         print("-- RECIEVED REQUEST --\r\n" + line.decode('utf-8'))
-    # LO QUE RECIBO REVISAR SI DEBERIA ESTAR AQUI O ABAJO
 
         if line_str[0] == "REGISTER":
-            
+
             if "Digest" not in line_str:
                 self.nonce.append(str(random.randint(000000000000000000000,
                                                      99999999999999999999)))
@@ -166,19 +166,22 @@ class SIPHandler(socketserver.DatagramRequestHandler):
                             "Digest nonce=" + self.nonce[0]
                 sents_log(self.client_address, log_file, s_content)
             else:
+
                 hash_recieved = line_str[-1].split("=")[1]
                 user = line_str[1].split(":")[1]
                 my_digest = hashlib.md5()
                 my_digest.update(bytes(self.nonce[0], "utf-8"))
                 my_digest.update(bytes(find_password(user), "utf-8"))
                 my_digest.digest
+
                 if hash_recieved == my_digest.hexdigest():
                     port = line_str[1].split(":")[2]
                     expire = line_str[4]
+                    ip = self.client_address[1]
                     reg_time = time.strftime("%Y-%m-%d %H:%M:%S",
                                              time.gmtime(time.time()))
-                    registered = user + " 127.0.0.1 " + port + " " + reg_time\
-                                        + " " + expire + "\n"
+                    registered = user + " " + str(ip) + " " + port + " " +\
+                                 reg_time + " " + expire + "\n"
                     make_users(registered)  # Making my database users txt.
                     time_to_del = time.strftime("%Y-%m-%d %H:%M:%S",
                                                 time.gmtime(time.time() +
@@ -189,67 +192,61 @@ class SIPHandler(socketserver.DatagramRequestHandler):
                                          "port": port,
                                          "expire_time": expire,
                                          "expires": time_to_del}
-                    print(self.my_dic, " mis usuarios activos")
                     if int(expire) == 0:
                         del self.my_dic[user]
-                        print(self.my_dic, " asi ahora borrado")
                     self.expired()
                     self.register2json()
                 else:
-                    print("FALSO NO ME COINCIDEN USER NOT FOUND?")
+                    print("-- ALERT! -- User password is incorrect.\n")
                     self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
                     s_content = "SIP/2.0 400 Bad Request"
                     sents_log(self.client_address, log_file, s_content)
+
                 self.nonce.clear()
 
-        elif line_str[0] == "INVITE":
-        
-            self.json2registered()
-            self.expired()
-            user_to_send = line_str[1].split(":")[1]
-            if self.find_register(user_to_send):
-                ip_serv = self.my_dic[user_to_send]["address"]
-                port_serv = self.my_dic[user_to_send]["port"]
-                recieved = send_to_uaserver(ip_serv, port_serv, 
-                                            line.decode('utf-8'))
-                self.wfile.write(bytes(recieved, "utf-8"))
-                log_hash = (" ").join(recieved.split())
-                sents_log(self.client_address, log_file, log_hash)
-            else:
-                self.wfile.write(b"SIP/2.0 404 User Not Found\r\n\r\n")
-                s_content = "SIP/2.0 404 User Not Found"
-                sents_log(self.client_address, log_file, s_content)
+        elif line_str[0] == "INVITE" or line_str[0] == "BYE":
 
-        elif line_str[0] == "ACK":
-            
-            self.json2registered()
-            self.expired()
-            user_to_send = line_str[1].split(":")[1]
-            print(user_to_send)
-            if self.find_register(user_to_send):
-                ip_serv = self.my_dic[user_to_send]["address"]
-                port_serv = self.my_dic[user_to_send]["port"]
-                recieved = send_to_uaserver(ip_serv, port_serv, 
-                                            line.decode('utf-8'))
-            else:
-                self.wfile.write(b"SIP/2.0 404 User Not Found\r\n\r\n")
-                s_content = "SIP/2.0 404 User Not Found"
-                sents_log(self.client_address, log_file, s_content)
-
-        elif line_str[0] == "BYE":
-            
             self.json2registered()
             self.expired()
             if len(line_str) != 2:
-                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-                s_content = "SIP/2.0 200 OK"
+                user_to_send = line_str[1].split(":")[1]
+                if self.find_register(user_to_send):
+                    ip_serv = self.my_dic[user_to_send]["address"]
+                    port_serv = self.my_dic[user_to_send]["port"]
+                    recieved = send_to_uaserver(ip_serv, port_serv,
+                                                line.decode('utf-8'))
+                    self.wfile.write(bytes(recieved, "utf-8"))
+                    log_hash = (" ").join(recieved.split())
+                    sents_log(self.client_address, log_file, log_hash)
+                else:
+                    self.wfile.write(b"SIP/2.0 404 User Not Found\r\n\r\n")
+                    s_content = "SIP/2.0 404 User Not Found"
+                    sents_log(self.client_address, log_file, s_content)
             else:
                 self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
                 s_content = "SIP/2.0 400 Bad Request"
-            sents_log(self.client_address, log_file, s_content)
-        
+                sents_log(self.client_address, log_file, s_content)
+
+        elif line_str[0] == "ACK":
+
+            self.json2registered()
+            self.expired()
+            user_to_send = line_str[1].split(":")[1]
+            if self.find_register(user_to_send):
+                ip_serv = self.my_dic[user_to_send]["address"]
+                port_serv = self.my_dic[user_to_send]["port"]
+                recieved = send_to_uaserver(ip_serv, port_serv,
+                                            line.decode('utf-8'))
+            else:
+                self.wfile.write(b"SIP/2.0 404 User Not Found\r\n\r\n")
+                s_content = "SIP/2.0 404 User Not Found"
+                sents_log(self.client_address, log_file, s_content)
+
         elif line_str[0] != "":
-            
+
+            self.json2registered()
+            self.expired()
+
             if line_str[0] == "register" or line_str[0] == "invite" or\
                line_str[0] == "bye":  # Avoiding lower cases methods
                 self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
@@ -259,7 +256,6 @@ class SIPHandler(socketserver.DatagramRequestHandler):
                 self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
                 s_content = "SIP/2.0 405 Method Not Allowed"
                 sents_log(self.client_address, log_file, s_content)
-
 
     def expired(self):
         """Method that checks if there's an old client in my_dic."""
@@ -272,7 +268,7 @@ class SIPHandler(socketserver.DatagramRequestHandler):
         for client in expired_dic:
             del self.my_dic[client]
         return self.my_dic
-        
+
     def find_register(self, user):
         """Method that checks if there's a client in my active clients."""
         Found = False
@@ -280,6 +276,18 @@ class SIPHandler(socketserver.DatagramRequestHandler):
             if client == user:
                 return True
         return Found
+
+
+def open_log(config):
+    """Opening my log file previously opened by the uaclient."""
+    try:
+        log_file = open(config[-1])
+        log_file = open(config[-1], "a")
+    except FileNotFoundError:   # When the file does not exists.
+        log_file = open(config[-1], "w")
+        log_file.write(str(actual_time()) + " Starting...\n")
+    return log_file
+
 
 if __name__ == "__main__":
     try:
@@ -291,12 +299,7 @@ if __name__ == "__main__":
         parser.setContentHandler(cHandler)
         parser.parse(open(CONFIG))
         config = cHandler.get_config()
-        try:
-            log_file = open(config[-1])
-            log_file = open(config[-1], "a")
-        except FileNotFoundError:   # When the file does not exists.
-            log_file = open(config[-1], "w")
-            log_file.write(str(actual_time()) + " Starting...\n")
+        log_file = open_log(config)
         serv = socketserver.UDPServer((config[1], int(config[2])), SIPHandler)
         print("Server " + config[0] + " listening at port " + config[2] +
               "...\n")
