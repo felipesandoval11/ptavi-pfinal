b6c62b6f2d3cf2a0b91ea7cc93a1e09684ba9ecf
Felipe Sandoval <felipesandovalsibada@outlook.com>
2017-01-11 12:46:40 +0100

    terminada la practica final.

--- uaserver.py
+++ uaserver.py
@@ -9,6 +9,7 @@ import socketserver
 import sys
 import os
 import time
+import threading
 
 
 class ConfigHandler(ContentHandler):
@@ -91,6 +92,18 @@ def recieved_log(config, log_file, sip_data):
                    ":" + config[6] + ": " + sip_data + "\n")
 
 
+def cvlc(dest, port):
+    """To execute CVLC in Thread."""
+    command = 'cvlc rtp://@' + dest + ':' + port
+    os.system(command)
+
+
+def mp32rtp(dest, port, music):
+    """To execute mp32rtp in Thread."""
+    command = "./mp32rtp -i " + dest + " -p " + port + " < " + music
+    os.system(command)
+
+
 class SIPHandler(socketserver.DatagramRequestHandler):
     """Main handler of an UA Server."""
 
@@ -105,31 +118,50 @@ class SIPHandler(socketserver.DatagramRequestHandler):
 
         if line_str[0] == "INVITE":
 
-            self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
-            self.wfile.write(b"SIP/2.0 180 Ringing\r\n\r\n")
-            self.wfile.write(b"SIP/2.0 200 OK\r\n")
-            self.wfile.write(bytes("Content-Type: application/sdp\r\n\r\n" +
-                                   "v=0\r\n" + "o=" + str(config[0]) + " " +
-                                   str(config[2]) + "\r\ns=PracticaFinal\r\n" +
-                                   "t=0\r\nm=audio " + str(config[4]) +
-                                   " RTP\r\n\r\n", 'utf-8'))
-            s_content = "SIP/2.0 100 Trying SIP/2.0 180 Ringing " +\
-                        "SIP/2.0 200 OK Content-Type: application/sdp v=0 " +\
-                        "o=" + config[0] + " " + config[2] + " s=Practica" +\
-                        "Final t=0 m=audio " + config[4] + " RTP"
-            sents_log(config, log_file, s_content)
-
             # making my list for rtp destination EP
             self.rtp_user.append(line_str[10])
             self.rtp_user.append(line_str[14])
             self.rtp_user.append(line_str[9].split("=")[1])
 
+            if threading.Thread(target=mp32rtp,
+                                args=(self.rtp_user[0], self.rtp_user[1],
+                                      config[-1])).is_alive():
+
+                self.wfile.write(bytes("SIP/2.0 480 Temporarily" +
+                                       " Unavailable\r\n\r\n", 'utf-8'))
+                s_content = "SIP/2.0 Temporarily Unavailable"
+
+            else:
+
+                self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
+                self.wfile.write(b"SIP/2.0 180 Ringing\r\n\r\n")
+                self.wfile.write(b"SIP/2.0 200 OK\r\n")
+                self.wfile.write(bytes("Content-Type: application/sdp\r\n" +
+                                       "\r\nv=0\r\n" + "o=" + str(config[0]) +
+                                       " " + str(config[2]) +
+                                       "\r\ns=PracticaFinal\r\n" +
+                                       "t=0\r\nm=audio " + str(config[4]) +
+                                       " RTP\r\n\r\n", 'utf-8'))
+                s_content = "SIP/2.0 100 Trying SIP/2.0 180 Ringing " +\
+                            "SIP/2.0 200 OK Content-Type: application/sdp " +\
+                            "v=0 o=" + config[0] + " " + config[2] +\
+                            " s=Practica Final t=0 m=audio " + config[4] +\
+                            " RTP"
+
+            sents_log(config, log_file, s_content)
+
         elif line_str[0] == "ACK":
 
             print("-- SENDING AUDIO --\n")
-            send = "./mp32rtp -i " + self.rtp_user[0] + " -p " +\
-                   self.rtp_user[1] + " < " + config[-1]
-            os.system(send)
+            # advanced part with Threads
+            thr_mp32rtp = threading.Thread(target=mp32rtp,
+                                           args=(self.rtp_user[0],
+                                                 self.rtp_user[1], config[-1]))
+            thr_cvlc = threading.Thread(target=cvlc, args=(self.rtp_user[0],
+                                                           self.rtp_user[1]))
+            thr_mp32rtp.start()
+            time.sleep(0.2)
+            thr_cvlc.start()
             log_file.write(str(actual_time()) + " Sent to " +
                            self.rtp_user[0] + ":" + self.rtp_user[1] +
                            ": AUDIO FILE " + config[-1] + "\n")
@@ -137,6 +169,8 @@ class SIPHandler(socketserver.DatagramRequestHandler):
 
         elif line_str[0] == "BYE":
 
+            os.system('killall mp32rtp 2> /dev/null')
+            os.system('killall vlc 2> /dev/null')
             self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
             s_content = "SIP/2.0 200 OK"
             sents_log(config, log_file, s_content)
